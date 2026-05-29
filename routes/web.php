d99094f436c22ebf5e14e76e95affc7f469824c1
Bigotes <ntsav1801@gmail.com>
2026-05-29 03:23:09 -0300

    fix: corregir 404 en la página de inicio

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use App\Http\Controllers\ProductoController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect()->route('productos.index');
+});
+
 Route::resource('productos', ProductoController::class);
