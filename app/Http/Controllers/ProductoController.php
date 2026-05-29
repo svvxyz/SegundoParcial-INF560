1c8691e376aa4158ea62b19338f8d55f23469d35
Bigotes <ntsav1801@gmail.com>
2026-05-28 23:54:40 -0400

    Parte 4: Rutas y controlador resource

---
+++ app/Http/Controllers/ProductoController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use App\Models\Producto;
+use Illuminate\Http\Request;
+use App\Http\Requests\ProductoRequest;
+
+class ProductoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $productos = Producto::with('categoria')->get();
+
+        return view('productos.index', compact('productos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $categorias = Categoria::all();
+
+        return view('productos.create', compact('categorias'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ProductoRequest $request)
+    {
+        Producto::create($request->all());
+
+        return redirect()->route('productos.index')->with('success', 'El producto se creo correctamente.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Producto $producto)
+    {
+        return view('productos.show', compact('producto'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Producto $producto)
+    {
+        $categorias = Categoria::all();
+
+        return view('productos.edit', compact('producto', 'categorias'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ProductoRequest $request, Producto $producto)
+    {
+        $producto->update($request->all());
+
+        return redirect()->route('productos.index')->with('success', 'El producto se actualizo correctamente.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Producto $producto)
+    {
+        $producto->delete();
+
+        return redirect()->route('productos.index')->with('success', 'El producto se elimino correctamente.');
+    }
+}
