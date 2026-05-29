c5efd6cc9382191cec37a471729ccbb14db19309
Bigotes <ntsav1801@gmail.com>
2026-05-29 00:04:34 -0400

    Parte 5: Validacion con form request

--- app/Http/Requests/ProductoRequest.php
+++ app/Http/Requests/ProductoRequest.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class ProductoRequest extends FormRequest
 {
@@ -12,18 +12,70 @@ class ProductoRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, ValidationRule|array<mixed>|string>
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'categoria_id' => [
+                'required',
+                'exists:categorias,id'
+            ],
+
+            'nombre' => [
+                'required',
+                'string',
+                'max:150'
+            ],
+
+            'sku' => [
+                'required',
+                Rule::unique('productos')->ignore($this->producto)
+            ],
+
+            'precio' => [
+                'required',
+                'numeric',
+                'min:0'
+            ],
+
+            'stock' => [
+                'required',
+                'integer',
+                'min:0'
+            ],
+
+            'disponible' => [
+                'nullable',
+                'boolean'
+            ]
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'categoria_id.required' => 'Debe seleccionar una categoría.',
+            'categoria_id.exists' => 'La categoría seleccionada no es válida.',
+
+            'nombre.required' => 'Debe ingresar un nombre para el producto.',
+
+            'precio.required' => 'Debe ingresar un precio.',
+            'precio.numeric' => 'El precio ingresado debe ser un número.',
+            'precio.min' => 'El precio debe ser mayor o igual a 0.',
+
+            'stock.required' => 'Debe indicar la cantidad en stock.',
+            'stock.integer' => 'La cantidad de stock debe ser un número entero.',
+            'stock.min' => 'El stock no puede tener valores negativos.',
+
+            'sku.required' => 'El código SKU es obligatorio.',
+            'sku.unique' => 'Ya existe un producto con ese SKU.'
         ];
     }
 }
